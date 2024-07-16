72fc1d9446b9a5d30b5adba1728feda48b7b2516
Abu Ghalib <abughalib63@hotmail.com>
2024-07-16 15:50:19 +0530

    added preferences

--- src/preferences/index.tsx
+++ src/preferences/index.tsx
@@ -1,3 +1,9 @@
+import { useRef } from "react";
+import { useTypedSelector } from "../hooks/use-typed-selector";
+import { useActions } from "../hooks/use-actions";
+import { changeInferenceTopP } from "../state/action-creators";
+import "./preference.css";
+
 interface ModalProps {
   isOpen: boolean;
   onClose: () => void;
@@ -7,27 +13,190 @@ export const PreferencesWindow: React.FC<ModalProps> = ({
   isOpen,
   onClose,
 }) => {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  const currentTable = useTypedSelector(
+    (state) => state.knowledgeReducer?.table_name || ""
+  );
+
+  const closeModal = (e: React.MouseEvent) => {
+    if (modalRef.current && e.target === modalRef.current) {
+      onClose();
+    }
+  };
+
+  const max_similar_search = useTypedSelector(
+    (state) => state.preferenceReducer?.max_similar_search
+  );
+
+  const upper_chunk = useTypedSelector(
+    (state) => state.preferenceReducer?.upper_chunk
+  );
+
+  const lower_chunk = useTypedSelector(
+    (state) => state.preferenceReducer?.lower_chunk
+  );
+
+  const temperature = useTypedSelector(
+    (state) => state.preferenceReducer?.inference_temperature
+  );
+
+  const seed = useTypedSelector(
+    (state) => state.preferenceReducer?.inference_seed
+  );
+
+  const top_p = useTypedSelector(
+    (state) => state.preferenceReducer?.inference_top_p
+  );
+
+  const repeat_penalty = useTypedSelector(
+    (state) => state.preferenceReducer?.repeat_penalty
+  );
+
+  const repeat_last_n = useTypedSelector(
+    (state) => state.preferenceReducer?.repeat_last_n
+  );
+
+  const {
+    changeMaxSimilarSearch,
+    changeInferenceTemperature,
+    changeInferenceSeed,
+    changeUpperChunk,
+    changeLowerChunk,
+    changeRepeatLastN,
+    changeRepeatPenalty,
+  } = useActions();
+
+  const handleSave = () => {
+    onClose();
+  };
+
   if (!isOpen) return null;
   return (
     <div
       className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full"
       id="upload_modal"
-      onClick={onClose}
+      ref={modalRef}
+      onClick={closeModal}
     >
       <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white dark:bg-black dark:text-white">
         <div className="mt-3 text-center">
-          <h3 className="text-lg leading-6 font-medium">Preferences</h3>
-          <div className="mt-2 px-7 py-3">
-            <div className="my-1 flex justify-start">
-              <ul>
-                <li>
-                  <label className="flex flex-row mx-2">
-                    <input className="mx-2" type="radio" />
-                    <p>Test</p>
-                  </label>
-                </li>
-              </ul>
-            </div>
+          <h3 className="text-lg leading-6 font-medium">Your Preferences</h3>
+          <div className="mt-2 px-7 py-3 preference-table">
+            <label className="flex justify-between flex-row">
+              Knowledge Base:{" "}
+              <input
+                className="rounded bg-slate-600 dark:bg-slate-800"
+                value={currentTable}
+                type="text"
+                disabled
+              />
+            </label>
+            <label className="flex justify-between flex-row">
+              Max Similar Search:{" "}
+              <input
+                className="rounded outline-0 dark:bg-gray-700 dark:text-white"
+                value={max_similar_search}
+                type="number"
+                onChange={(e) => {
+                  changeMaxSimilarSearch(Math.round(+e.target.value) || 3);
+                }}
+                pattern="\d+"
+              />
+            </label>
+            <label className="flex justify-between flex-row">
+              Upper Chunk:{" "}
+              <input
+                className="rounded outline-0 dark:bg-gray-700 dark:text-white"
+                value={upper_chunk}
+                type="number"
+                onChange={(e) => {
+                  changeUpperChunk(Math.round(+e.target.value) || 0);
+                }}
+                pattern="\d+"
+              />
+            </label>
+            <label className="flex justify-between flex-row">
+              Lower Chunk:{" "}
+              <input
+                className="rounded outline-0 dark:bg-gray-700 dark:text-white"
+                value={lower_chunk}
+                type="number"
+                onChange={(e) => {
+                  changeLowerChunk(Math.round(+e.target.value) || 0);
+                }}
+                pattern="\d+"
+              />
+            </label>
+            <label className="flex justify-between flex-row">
+              Temperature:{" "}
+              <input
+                className="rounded outline-0 dark:bg-gray-700 dark:text-white"
+                value={temperature}
+                type="number"
+                onChange={(e) => {
+                  changeInferenceTemperature(Math.round(+e.target.value) || 0);
+                }}
+                pattern="0|[1-9]\d*"
+              />
+            </label>
+            <label className="flex justify-between flex-row">
+              Seed:{" "}
+              <input
+                className="rounded outline-0 dark:bg-gray-700 dark:text-white"
+                value={seed}
+                type="number"
+                onChange={(e) => {
+                  changeInferenceSeed(Math.round(+e.target.value) || 0);
+                }}
+                pattern="\d{1,6}"
+              />
+            </label>
+            <label className="flex justify-between flex-row">
+              Top P:{" "}
+              <input
+                className="rounded outline-0 dark:bg-gray-700 dark:text-white"
+                value={top_p}
+                type="number"
+                onChange={(e) => {
+                  changeInferenceTopP(Math.round(+e.target.value) || 0);
+                }}
+                pattern="\d"
+              />
+            </label>
+            <label className="flex justify-between flex-row">
+              Repeat Penalty:{" "}
+              <input
+                className="rounded outline-0 dark:bg-gray-700 dark:text-white"
+                value={repeat_penalty}
+                type="number"
+                onChange={(e) => {
+                  changeRepeatPenalty(Math.round(+e.target.value) || 0);
+                }}
+                pattern="\d\.\d"
+              />
+            </label>
+            <label className="flex justify-between flex-row">
+              Repeat Last N:{" "}
+              <input
+                className="rounded outline-0 dark:bg-gray-700 dark:text-white"
+                value={repeat_last_n}
+                type="number"
+                onChange={(e) => {
+                  changeRepeatLastN(Math.round(+e.target.value) || 0);
+                }}
+                pattern="\d{2}"
+              />
+            </label>
+          </div>
+          <div className="items-center px-4 py-3">
+            <button
+              id="upload_button"
+              className="px-4 py-2 bg-blue-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              onClick={handleSave}
+            >
+              Save
+            </button>
           </div>
         </div>
       </div>
